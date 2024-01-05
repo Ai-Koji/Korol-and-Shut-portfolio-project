27de0dbaf25b8c08936ed047195e76fecbfebd1e
Lost-S0ul <Kireev.ivan.8@gmail.com>
2024-01-05 15:29:59 +0700

    feat: added new model for "disks" page

--- backend/disks/admin.py
+++ backend/disks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class DisksAdmin(admin.ModelAdmin):
+    filter_horizontal = ('audio', 'images')
+
+admin.site.register(Disk, DisksAdmin)
+admin.site.register(Audio)
+admin.site.register(Image)
--- backend/disks/models.py
+++ backend/disks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Audio(models.Model):
+    name = models.CharField(max_length=50)
+    file = models.FileField(upload_to="media/audio/")
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    file = models.FileField(upload_to="media/images/")
+    def __str__(self):
+        return self.name
+
+class Disk(models.Model):
+    name = models.CharField(max_length=50)
+    about = models.TextField()
+    audio = models.ManyToManyField(Audio, blank=True)
+    images = models.ManyToManyField(Image, blank=True)
+    def __str__(self):
+        return self.name
