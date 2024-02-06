8a74b2ef1f2fef9b385162595681be63d8db064d
Ai-Koji <kireev.ivan.8@gmail.com>
2024-02-06 15:04:55 +0700

    refactor: refactor code

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from biography.models import *
+from disks.models import *
 
 def main(request):
     member_list = Member.objects.filter(is_main=True)
+    disks_list = Disk.objects.all()
 
-    return render(request, "main.html", {"member_list": member_list})
+    return render(request, "main.html", {"member_list": member_list, "disks_list": disks_list})
