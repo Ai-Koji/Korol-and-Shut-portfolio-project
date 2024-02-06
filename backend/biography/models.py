4dd1d1310c084d58c40a30c50e7fd33f4246a572
Ai-Koji <kireev.ivan.8@gmail.com>
2024-02-06 21:11:51 +0700

    feat: updated disk page

--- backend/biography/models.py
+++ backend/biography/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Member(models.Model):
     name = models.CharField(max_length=50)
-    image = models.FileField(upload_to="image/member/", blank=True)
+    image = models.FileField(upload_to="images/member/", blank=True)
     role = models.CharField(max_length=20, blank=True)
     about = models.TextField(blank=True)
     is_main = models.BooleanField(blank=True)
